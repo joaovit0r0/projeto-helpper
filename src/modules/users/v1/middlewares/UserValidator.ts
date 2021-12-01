59141eaf71b02357afba9946cf5d52038472e838
joaovit0r0 <joao.teixeira2@estudante.ufla.br>
2021-12-01 16:48:14 -0300

    criando validator para login

--- src/modules/users/v1/middlewares/UserValidator.ts
+++ src/modules/users/v1/middlewares/UserValidator.ts
@@ -1,6 +1,8 @@
 // Libraries
 import { RequestHandler } from 'express';
 import { Schema } from 'express-validator';
+import crypto from 'crypto';
+import { EnumCrypto } from '../../../../models/EnumCrypto';
 
 // Repositories
 import { UserRepository } from '../../../../library/database/repository/UserRepository';
@@ -23,10 +25,28 @@ export class UserValidator extends BaseValidator {
      * Schema para validação no controller de usuários
      */
     private static model: Schema = {
-        name: BaseValidator.validators.name,
         id: {
             ...BaseValidator.validators.id(new UserRepository()),
             errorMessage: 'Usuário não encontrado'
+        },
+        email: {
+            ...BaseValidator.validators.emailBase,
+            custom: {
+                options: async (_: string, { req }) => {
+                    let check = false;
+
+                    if (req.body.email) {
+                        const userRepository: UserRepository = new UserRepository();
+                        const user: User | undefined = await userRepository.findByEmail(req.body.email);
+
+                        req.body.userRef = user;
+
+                        check = !!user;
+                    }
+
+                    return check ? Promise.resolve() : Promise.reject();
+                }
+            }
         },
         duplicate: {
             errorMessage: 'Usuário já existe',
@@ -34,11 +54,31 @@ export class UserValidator extends BaseValidator {
                 options: async (_: string, { req }) => {
                     let check = false;
 
-                    if (req.body.name) {
+                    if (req.body.email) {
                         const userRepository: UserRepository = new UserRepository();
-                        const user: User | undefined = await userRepository.findByName(req.body.name);
+                        const user: User | undefined = await userRepository.findByEmail(req.body.email);
 
                         check = user ? req.body.id === user.id.toString() : true;
+                    }
+
+                    return check ? Promise.resolve() : Promise.reject();
+                }
+            }
+        },
+        password: {
+            errorMessage: 'Senha incorreta',
+            custom: {
+                options: async (_: string, { req }) => {
+                    let check = false;
+
+                    if (req.body.userRef) {
+                        const iv = req.body.userRef.initializationVector;
+                        const cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(EnumCrypto.KEY), iv.b);
+                        let encrypted = cipher.update(req.body.password);
+                        encrypted = Buffer.concat([encrypted, cipher.final()]);
+                        req.body.password = encrypted;
+
+                        check = req.body.password === req.body.userRef.password;
                     }
 
                     return check ? Promise.resolve() : Promise.reject();
@@ -54,8 +94,8 @@ export class UserValidator extends BaseValidator {
      */
     public static post(): RequestHandler[] {
         return UserValidator.validationList({
-            name: UserValidator.model.name,
-            duplicate: UserValidator.model.duplicate
+            email: UserValidator.validators.emailBase
+            // password: UserValidator.model.password
         });
     }
 
