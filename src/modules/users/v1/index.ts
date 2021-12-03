2ff8777460fb4509ceac05b109e6b95d50effdd9
joaovit0r0 <joao.teixeira2@estudante.ufla.br>
2021-12-03 18:35:56 -0300

    adicionando export

--- src/modules/users/v1/index.ts
+++ src/modules/users/v1/index.ts
@@ -3,3 +3,4 @@ export { UserController } from './controllers/UserController';
 
 // Middlewares
 export { UserValidator } from './middlewares/UserValidator';
+export { TaskValidator } from './middlewares/TaskValidator';
