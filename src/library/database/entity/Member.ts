5251bfcb9ff189faba14a232263520ad3114df2f
GabrielPradoC <69232929+GabrielPradoC@users.noreply.github.com>
2021-12-08 14:10:25 -0300

    feat: Add member model

---
+++ src/library/database/entity/Member.ts
@@ -0,0 +1,45 @@
+import { Entity, ObjectID, ObjectIdColumn, Column, BeforeInsert, BeforeUpdate, BaseEntity } from 'typeorm';
+
+@Entity()
+export class Member extends BaseEntity {
+    @ObjectIdColumn() // Alterar para @PrimaryGeneratedColumn em caso de banco diferente do MongoDB
+    public id: ObjectID;
+
+    @Column({ unique: true })
+    public name: string;
+
+    @Column()
+    public parentId: string;
+
+    @Column()
+    public photo: string;
+
+    @Column()
+    public birthdate: Date;
+
+    @Column()
+    public allowance: number;
+
+    @Column()
+    public createdAt: Date;
+
+    @Column()
+    public updatedAt: Date;
+
+    @BeforeInsert()
+    public setCreateDate(): void {
+        this.createdAt = new Date();
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    public setUpdateDate(): void {
+        this.updatedAt = new Date();
+    }
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    public checkAllowance(): void {
+        this.allowance = Math.max(0, this.allowance);
+    }
+}
