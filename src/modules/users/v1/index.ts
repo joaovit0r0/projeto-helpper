35fcc09adb8d205af307bfa2bd189b417c06fe34
GabrielPradoC <69232929+GabrielPradoC@users.noreply.github.com>
2021-12-14 21:38:29 -0300

    Revert "feat: Add lists endpoint"
    This reverts commit eb03bcd8504451a78867bdd0d5115ce839a7bd78.

--- src/modules/users/v1/index.ts
+++ src/modules/users/v1/index.ts
@@ -1,9 +1,7 @@
 // Controllers
 export { UserController } from './controllers/UserController';
 export { TaskController } from './controllers/TaskController';
-export { ListController } from './controllers/ListController';
 
 // Middlewares
 export { UserValidator } from './middlewares/UserValidator';
 export { TaskValidator } from './middlewares/TaskValidator';
-export { ListValidator } from './middlewares/ListValidator';
