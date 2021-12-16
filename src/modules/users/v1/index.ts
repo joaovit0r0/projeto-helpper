5ff8f876d8d22ff20ab26e3937be6e1ea72c6c40
GabrielPradoC <69232929+GabrielPradoC@users.noreply.github.com>
2021-12-16 00:45:09 -0300

    fix: Update index.ts

--- src/modules/users/v1/index.ts
+++ src/modules/users/v1/index.ts
@@ -1,11 +1,11 @@
 // Controllers
 export { UserController } from './controllers/UserController';
-export { ListController } from './controllers/ListController';
+// export { ListController } from './controllers/ListController';
 export { MemberController } from './controllers/MemberController';
 export { TaskController } from './controllers/TaskController';
 
 // Middlewares
 export { UserValidator } from './middlewares/UserValidator';
 export { MemberValidator } from './middlewares/MemberValidator';
-export { ListValidator } from './middlewares/ListValidator';
+// export { ListValidator } from './middlewares/ListValidator';
 export { TaskValidator } from './middlewares/TaskValidator';
